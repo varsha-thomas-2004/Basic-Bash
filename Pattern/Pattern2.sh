d3e4f3f78c3f0a71fc9a1b1d669cc08e6f30e69c
Varsha Thomas <118676016+varsha-thomas-2004@users.noreply.github.com>
2024-07-04 22:53:28 +0530

    Pattern2

---
+++ Pattern/Pattern2.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#Pattern2
+
+echo Enter n
+read n
+for (( i=1 ; i<=$n ; i++))
+do
+	for (( j=1 ; j<=$i ; j++))
+	do
+		echo -ne '*'
+	done
+echo -e "\n"
+done
+
+: <<'END_COMMENT'
+Output
+
+Enter n
+3
+*
+
+**
+
+***
+END_COMMENT
